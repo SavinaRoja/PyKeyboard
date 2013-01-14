1ccaf6bdebdc82fd424a7b3af1861fc04936f1a5
Paul Barton <pablo.barton@gmail.com>
2013-01-14 14:48:02 -0500

    Major update to special key support. Handles most of standard X11

--- pykeyboard/unix.py
+++ pykeyboard/unix.py
@@ -116,7 +116,89 @@ class PyKeyboard(PyKeyboardMeta):
         if character in '<>?:"{}|~!@#$%^&*()_+':
             return True
         return False
-    
+
+    def special_key_assignment(self):
+        """
+        Determines the keycodes for common special keys on the keyboard. These
+        are integer values and can be passed to the other key methods.
+        Generally speaking, these are non-printable codes.
+        """
+        #This set of keys compiled using the X11 keysymdef.h file as reference
+        #They comprise a relatively universal set of keys, though there may be
+        #exceptions which may come up for other OSes and vendors. Countless
+        #special cases exist which are not handled here, but may be extended.
+        #TTY Function Keys
+        self.backspace_key = self.lookup_character_value('BackSpace')
+        self.tab_key = self.lookup_character_value('Tab')
+        self.linefeed_key = self.lookup_character_value('Linefeed')
+        self.clear_key = self.lookup_character_value('Clear')
+        self.return_key = self.lookup_character_value('Return')
+        self.enter_key = self.return_key  # Because many keyboards call it "Enter"
+        self.pause_key = self.lookup_character_value('Pause')
+        self.scroll_lock_key = self.lookup_character_value('Scroll_Lock')
+        self.sys_req_key = self.lookup_character_value('Sys_Req')
+        self.escape_key = self.lookup_character_value('Escape')
+        self.delete_key = self.lookup_character_value('Delete')
+        #Modifier Keys
+        self.shift_l_key = self.lookup_character_value('Shift_L')
+        self.shift_r_key = self.lookup_character_value('Shift_R')
+        self.shift_key = self.shift_l_key
+        self.alt_l_key = self.lookup_character_value('Alt_L')
+        self.alt_r_key = self.lookup_character_value('Alt_R')
+        self.alt_key = self.alt_l_key
+        self.control_l_key = self.lookup_character_value('Control_L')
+        self.control_r_key = self.lookup_character_value('Control_R')
+        self.control_key = self.control_l_key
+        self.caps_lock_key = self.lookup_character_value('Caps_Lock')
+        self.shift_lock_key = self.lookup_character_value('Shift_Lock')
+        self.meta_l_key = self.lookup_character_value('Meta_L')
+        self.meta_r_key = self.lookup_character_value('Meta_R')
+        self.super_l_key = self.lookup_character_value('Super_L')
+        self.super_r_key = self.lookup_character_value('Super_R')
+        self.hyper_l_key = self.lookup_character_value('Hyper_L')
+        self.hyper_r_key = self.lookup_character_value('Hyper_R')
+        #Cursor Control and Motion
+        self.home_key = self.lookup_character_value('Home')
+        self.up_key = self.lookup_character_value('Up')
+        self.down_key = self.lookup_character_value('Down')
+        self.left_key = self.lookup_character_value('Left')
+        self.right_key = self.lookup_character_value('Right')
+        self.end_key = self.lookup_character_value('End')
+        self.begin_key = self.lookup_character_value('Begin')
+        self.page_up_key = self.lookup_character_value('Page_Up')
+        self.page_down_key = self.lookup_character_value('Page_Down')
+        self.prior_key = self.lookup_character_value('Prior')
+        self.next_key = self.lookup_character_value('Next')
+        #Misc Functions
+        self.select_key = self.lookup_character_value('Select')
+        self.print_key = self.lookup_character_value('Print')
+        self.execute_key = self.lookup_character_value('Execute') 
+        self.insert_key = self.lookup_character_value('Insert')
+        self.undo_key = self.lookup_character_value('Undo')
+        self.redo_key = self.lookup_character_value('Redo')
+        self.menu_key = self.lookup_character_value('Menu')
+        self.find_key = self.lookup_character_value('Find')
+        self.cancel_key = self.lookup_character_value('Cancel')
+        self.help_key = self.lookup_character_value('Help')
+        self.break_key = self.lookup_character_value('Break')
+        self.mode_switch_key = self.lookup_character_value('Mode_switch')
+        self.script_switch_key = self.lookup_character_value('script_switch')
+        self.num_lock_key = self.lookup_character_value('Num_Lock')
+        #Keypad Keys: Dictionary structure
+        keypad = ['Space', 'Tab', 'Enter', 'F1', 'F2', 'F3', 'F4', 'Home',
+                  'Left', 'Up', 'Right', 'Down', 'Prior', 'Page_Up', 'Next',
+                  'Page_Down', 'End', 'Begin', 'Insert', 'Delete', 'Equal',
+                  'Multiply', 'Add', 'Separator', 'Subtract', 'Decimal',
+                  'Divide', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.keypad_keys = {k: self.lookup_character_value('KP_'+str(k)) for k in keypad}
+        #Function Keys/ Auxilliary Keys
+        #FKeys
+        self.function_keys = [None] + [self.lookup_character_value('F'+str(i)) for i in xrange(1,36)]
+        #LKeys
+        self.l_keys = [None] + [self.lookup_character_value('L'+str(i)) for i in xrange(1,11)]
+        #RKeys
+        self.r_keys = [None] + [self.lookup_character_value('R'+str(i)) for i in xrange(1,16)]
+
     def lookup_character_value(self, character):
         """
         Looks up the keysym for the character then returns the keycode mapping
