30696c6d7b9f899df09019961f4678dff026c813
Paul Barton <pablo.barton@gmail.com>
2013-01-29 17:01:44 -0500

    Adjusted to refer to x11

--- pykeyboard/__init__.py
+++ pykeyboard/__init__.py
@@ -3,7 +3,7 @@ The goal of PyKeyboard is to provide a cross-platform way to control the
 keyboard. It is intended to be complementary to the PyMouse module by
 pepijndevos and should work on the same dependencies.
 
-PyKeyboard should work on Windows, Mac, and any Unix that has xlib.
+PyKeyboard should work on Windows, Mac, and X11 systems.
 
 For more information about PyKeyboard, see:
 See http://github.com/SavinaRoja/PyKeyboard
@@ -24,4 +24,4 @@ elif sys.platform == 'win32':
     from windows import PyKeyboard
 
 else:
-    from unix import PyKeyboard
+    from x11 import PyKeyboard
